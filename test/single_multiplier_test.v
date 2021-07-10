1e1c89b8bfee84d984353b57cb5b0b652fb22d09
hamedrashidpour <hamedrashid1421@gmail.com>
2021-07-10 20:41:48 +0430

    single_multiplier unit test

--- test/single_multiplier_test.v
+++ test/single_multiplier_test.v
@@ -64,12 +64,43 @@ module single_multiplier_test;
 		clk = 0;
 		rst = 0;
 		
-		#13
+		#100
 		rst = 1;
-		input_a = 32'b01000001010001001100110011001101; //12.3
-		input_b = 32'b01000001011001011000010100011111; //14.345
 		input_a_stb = 1;
 		input_b_stb = 1;
+		input_a = 32'b01000001010001001100110011001101; //12.3
+		input_b = 32'b01000001011001011000010100011111; //14.345
+		// output_z = 176.44345 its ok
+
+		#300
+		rst = 0;
+		input_a = 32'b01000001010001001100110011001101; //12.3
+		input_b = 32'b01000001011001011000010100011111; //14.345
+		// rst = 0 ----> module stay in default state its ok
+
+		#300
+		rst = 1;
+		input_a = 32'b10111110000101100000000000000000; //-0.146484375
+		input_b = 32'b00111100011000000000000000000000; // 0.013671875
+		// output_z = -0.00200271606445 its ok 
+
+		#300
+		input_a_stb = 0;
+		input_a = 32'b10111110000101100000000000000000; //-0.146484375
+		input_b = 32'b00111100011000000000000000000000; // 0.013671875
+		// first input is not stable so module dont calculate anything stay in first state (get_a) its ok
+
+		#300
+		input_a_stb = 1;
+		input_a = 32'b11001111100100000000000000000000; //-4831838208.0
+		input_b = 32'b11001101100111100000000000000000; //-331350016.0
+		// output_z = 1.60102966753 * 10 ^ 18 its ok 
+
+		#300
+		input_b_stb = 0;
+		input_a = 32'b11001111100100000000000000000000; //-4831838208.0
+		input_b = 32'b11001101100111100000000000000000; //-331350016.0
+		// second input is not stable so module dont start calculating and stay in second state (get_b) its ok
 
 	end
       
