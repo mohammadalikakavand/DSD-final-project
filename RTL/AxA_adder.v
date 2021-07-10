3e64bf1a82d84f9ef2cea36162eb79d786412f1d
mohammadalikakavand <malikakavand@gmail.com>
2021-07-10 13:02:15 +0430

    edit AxA_adder

--- RTL/AxA_adder.v
+++ RTL/AxA_adder.v
@@ -34,33 +34,28 @@ input_Start, // for starting the addition progress
 
 input_C_Ack, //acknowledging "output read."
 
-output_AB_Ack, // acknowledging "inputs read."
 output_Stable, // acknowledging "the output is ready!"
 
 output_C11,
 output_C12,
 output_C21,
-output_C22,
- );
+output_C22);
 
     input [31:0] input_A11, input_A12, input_A21, input_A22;
     input [31:0] input_B11, input_B12, input_B21, input_B22;
-    output reg output_AB_Ack;
 
-    output reg [31:0] output_C11, output_C12, output_C21, output_C22;
-    output reg output_Stable;
+    output [31:0] output_C11, output_C12, output_C21, output_C22;
+    output output_Stable;
     input input_C_Ack;
 
     input input_Clk;
     input input_Reset;
     input input_Start;
 
-    reg [1:0] state;
-    parameter waiting     = 2'd0;
-              calculating = 2'd1;
-              done        = 2'd2;
+    wire [3:0] stable;
+    assign output_Stable =  stable[0] & stable[1] & stable[2] & stable[3];
 
-    adder(
+    adder adder_1(
     .clk(input_Clk),
     .reset(input_Reset), 
     .load(input_Start),
@@ -68,37 +63,37 @@ output_C22,
     .Number2(input_B11), 
     .result_ack(input_C_Ack),
     .Result(output_C11),
-    .result_ready(output_Stable));
+    .result_ready(stable[0]));
 
-    adder(
+    adder adder_2(
     .clk(input_Clk),
     .reset(input_Reset), 
     .load(input_Start),
     .Number1(input_A12), 
     .Number2(input_B12), 
     .result_ack(input_C_Ack),
-    .Result(output_C11),
-    .result_ready(output_Stable));
+    .Result(output_C12),
+    .result_ready(stable[1]));
 
-    adder(
+    adder adder_3(
     .clk(input_Clk),
     .reset(input_Reset), 
     .load(input_Start),
     .Number1(input_A21), 
     .Number2(input_B21), 
     .result_ack(input_C_Ack),
-    .Result(output_C11),
-    .result_ready(output_Stable));
+    .Result(output_C21),
+    .result_ready(stable[2]));
 
-    adder(
+    adder adder_4(
     .clk(input_Clk),
     .reset(input_Reset), 
     .load(input_Start),
     .Number1(input_A22), 
     .Number2(input_B22), 
     .result_ack(input_C_Ack),
-    .Result(output_C11),
-    .result_ready(output_Stable));          
+    .Result(output_C22),
+    .result_ready(stable[3]));          
 
 
 endmodule
