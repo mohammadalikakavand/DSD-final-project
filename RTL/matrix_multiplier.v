7db73e3b9d40ef1285bdca1060af0c67e84b3cfa
mohammadalikakavand <malikakavand@gmail.com>
2021-07-13 17:58:41 +0430

    implement matrix_multiplier

--- RTL/matrix_multiplier.v
+++ RTL/matrix_multiplier.v
@@ -21,44 +21,51 @@
 module matrix_multiplier
     #(parameter DATA_WIDTH=32,
       parameter ADDR_WIDTH=12, // memory address length
-      parameter MAX_LEN = 100; // matrices maximum dimensions
+      parameter MAX_LEN = 100, // matrices maximum dimensions
       parameter MAX_LEN_LOG = 7)
 
    (input [(DATA_WIDTH-1):0] memory_data_in,
-	input [(ADDR_WIDTH-1):0] memory_address
-	input we_a,
+	input [(ADDR_WIDTH-1):0] memory_address,
+	input write_enable,
+    input reset,
     input clk,
-	output [(DATA_WIDTH-1):0] memory_data_out
-    
+	output [(DATA_WIDTH-1):0] memory_data_out,
+    output result_ready
     );
 
+    wire data_a, data_b, addr_a, addr_b, we_a, we_b, is_working;
+    assign data_b = is_working? data_b_inner : memory_data_in;
+    assign addr_b = is_working? addr_b_inner : memory_address;
+    assign we_b = is_working? we_b_inner : write_enable;
+    assign q_b = is_working? q_b_inner : memory_data_out;
 
     memory my_memory(
-        .data_a(),
-        .data_b(),
-        .addr_a(),
-        .addr_b(),
-        .we_a(),
-        .we_b(),
-        .clk(),
-        .q_a(),
-        .q_b());
+        .data_a(data_a),
+        .data_b(data_b),
+        .addr_a(addr_a),
+        .addr_b(addr_b),
+        .we_a(we_a),
+        .we_b(we_b),
+        .clk(clk),
+        .q_a(q_a),
+        .q_b(q_b));
         defparam my_memory.DATA_WIDTH = DATA_WIDTH;
         defparam my_memory.ADDR_WIDTH = ADDR_WIDTH;
 
 
     multiplier my_multiplier(
-        .data_a(),
-        .data_b(),
-        .addr_a(),
-        .addr_b(),
-        .we_a(),
-        .we_b(),
-        .clk(),
-        .result_ready(),
-        .reset(),
-        .q_a(),
-        .q_b());
+        .data_a(data_a),
+        .data_b(data_b_inner),
+        .addr_a(addr_a),
+        .addr_b(addr_b_inner),
+        .we_a(we_a),
+        .we_b(we_b_inner),
+        .clk(clk),
+        .result_ready(result_ready),
+        .is_working(is_working),
+        .reset(reset),
+        .q_a(q_a),
+        .q_b(q_b_inner));
         defparam my_multiplier.DATA_WIDTH = DATA_WIDTH;
         defparam my_multiplier.ADDR_WIDTH = ADDR_WIDTH;
         defparam my_multiplier.MAX_LEN = MAX_LEN;
